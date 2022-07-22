be4986d49dfe6d60f9689cdd0cf592fe070b5900
Fernanda Eschallots <feschall@at-b1.msk.21-school.ru>
2022-07-22 16:41:01 +0300

    day02

---
+++ day02/src/ex00/Program.java
@@ -0,0 +1,18 @@
+package ex00;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		ArrayList<String[]> signatures = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
+		String filePath;
+		
+		SignaturesParser.parseFile(signatures);
+		while (!(filePath = scanner.next()).equals("42")) {
+			System.out.println(FileChecker.parseSignature(filePath, signatures));
+		}
+		scanner.close();
+	}
+}
